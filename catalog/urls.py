d05b9277a096e97a2abbe9ea97f77a9d564ebfca
User <shurbe96@mail.ru>
2025-06-17 00:10:31 +0700

    Домашняя работа 24

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from django.conf.urls.static import static
+from django.conf import settings
+from catalog.views import home, contacts, products_list, products_detail
+
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="index"),
+    path("home/", home, name="index"),
     path("contacts/", contacts, name="contacts"),
-]
+    path("products/", products_list, name="products_list"),
+    path("products/<int:pk>/", products_detail, name="products_detail")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
